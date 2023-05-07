97807c9bc09074ce2f8a16decbb53b4c8ee507b0
Renato Athaydes <renato@athaydes.com>
2023-05-07 22:30:02 +0200

    Dartle script improvements.
    Use global_ctx to deploy to GitHub Pages.

--- dartle.dart
+++ dartle.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:dartle/dartle.dart';
 import 'package:path/path.dart' as paths;
 
-final magFile = File(paths.join(homeDir(), '.magnanimous', 'mag'));
+String userHome() => homeDir() ?? failBuild(reason: 'Cannot find user HOME');
+
+final magFile = File(paths.join(userHome(), '.magnanimous', 'mag'));
 
 final magnanimousDownloadTask = Task(downloadMagnanimous,
     description: 'Download Magnanimous',
@@ -11,6 +13,7 @@ final magnanimousDownloadTask = Task(downloadMagnanimous,
 
 final magnanimousRunTask = Task(runMagnanimous,
     description: 'Builds the Dartle Website using Magnanimous',
+    argsValidator: const RunMagnanimousArgsValidator(),
     dependsOn: {magnanimousDownloadTask.name},
     runCondition: RunOnChanges(
         inputs: entities(['dartle.dart'], [DirectoryEntry(path: 'source')]),
@@ -20,55 +23,43 @@ void main(List<String> args) {
   run(args, tasks: {
     magnanimousRunTask,
     magnanimousDownloadTask,
-    createCleanTask(name: 'clean', tasks: [magnanimousRunTask]),
+    createCleanTask(tasks: [magnanimousRunTask]),
   }, defaultTasks: {
     magnanimousRunTask,
   });
 }
 
+String _osArch() {
+  if (Platform.isWindows) return 'windows-386.exe';
+  if (Platform.isLinux) return 'linux-386';
+  if (Platform.isMacOS) return 'darwin-amd64';
+  failBuild(reason: 'Unsupported OS: ${Platform.operatingSystem}');
+}
+
 Future<void> downloadMagnanimous(_) async {
-  final os = Platform.isWindows
-      ? 'windows-386.exe'
-      : Platform.isLinux
-          ? 'linux-386'
-          : Platform.isMacOS
-              ? 'darwin-amd64'
-              : error('Unsupported OS: ${Platform.operatingSystem}');
-  final req = await HttpClient()
-      .getUrl(Uri.parse('https://github.com/renatoathaydes/magnanimous/releases'
-          '/download/0.11.1/magnanimous-$os'));
-  req.persistentConnection = false;
-  req.headers.add('Accept', '*/*');
-  final res = await req.close();
-  if (res.statusCode == 200) {
-    await magFile.parent.create();
-    final handle = magFile.openWrite();
-    try {
-      await handle.addStream(res);
-      await handle.flush();
-    } finally {
-      await handle.close();
-    }
-    if (!Platform.isWindows) {
-      final proc =
-          await Process.run('chmod', ['+x', magFile.path], runInShell: true);
-      if (proc.exitCode != 0) error('Failed to make magnanimous executable');
-    }
-  } else
-    error('Unable to download Magnanimous: $res');
+  await magFile.parent.create();
+  final magStream = download(
+      Uri.parse('https://github.com/renatoathaydes/magnanimous/releases'
+          '/download/0.11.1/magnanimous-${_osArch()}'));
+  await magFile.writeBinary(magStream, makeExecutable: true);
 }
 
-Future<void> runMagnanimous(_) async {
-  final code = await exec(
-      Process.start(magFile.path, const ['-style', 'nord'], runInShell: true));
-  if (code != 0) {
-    throw DartleException(message: 'magnanimous exited with code $code');
-  }
+Future<int> runMagnanimous(List<String> args) async {
+  final contextArgs = args.contains('github')
+      ? ['-globalctx', '_github_global_context']
+      : const [];
+  return await execProc(Process.start(
+      magFile.path, [...contextArgs, '-style', 'nord'],
+      runInShell: true));
 }
 
-String homeDir() =>
-    Platform.environment['HOME'] ??
-    Platform.environment['USERPROFILE'] ??
-    error('Cannot find user HOME');
+class RunMagnanimousArgsValidator implements ArgsValidator {
+  const RunMagnanimousArgsValidator();
 
-Never error(String message) => throw DartleException(message: message);
+  @override
+  String helpMessage() => 'Accepts the "github" argument only.';
+
+  @override
+  bool validate(List<String> args) =>
+      args.isEmpty || (args.length == 1 && args.first == 'github');
+}
